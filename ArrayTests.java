e9106211ad6fdc53abcc0b4fe52de30d59c4e102
nixmai18 <kasibatlanimai@gmail.com>
2024-01-31 14:47:38 -0800

    Update ArrayExamples.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,18 +1,26 @@
 import static org.junit.Assert.*;
+
+import javax.management.openmbean.ArrayType;
+
 import org.junit.*;
 
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
+    int[] input2 = {3,4,5};
     ArrayExamples.reverseInPlace(input1);
+    ArrayExamples.reverseInPlace(input2);
     assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{5,4,3}, input2);
 	}
 
 
   @Test
   public void testReversed() {
     int[] input1 = { };
+    int[] input2 = {1,2,3};
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input2)); 
   }
 }
